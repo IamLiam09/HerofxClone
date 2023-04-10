71501d68265cabb1823c811ed4d7c14055353f6d
Ndubuisi William <princewill835@gmail.com>
2023-04-10 02:26:27 -0700

    partly complete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,64 +1,79 @@
 @extends('layout')
 @section('content')
-    <section>
-        <div>
-            <p>Raw spread and No commission broker</p>
-            <p>Trade Forex, Cryptos, and indicates with confidence and enjoy
-                a 1:500 Leverage</p>
-            <button>
-                Sign up for free
-            </button>
-        </div>
-        <div>
-            <img src="{{ URL("images/Herofx.png")}}" alt="mtscreens">
-        </div>
-    </section>
-    <div>
-        <div>
-            <img src="{{ URL("images/Herofx.png")}}" alt="mtscreens">
-        </div>
-        <div>
-            <img src="{{ URL("images/Herofx.png")}}" alt="mtscreens">
-        </div>
-        <div>
-            <img src="{{ URL("images/Herofx.png")}}" alt="mtscreens">
-        </div>
-        <div>
-            <img src="{{ URL("images/Herofx.png")}}" alt="mtscreens">
-        </div>
-    </div>
-    <section>
-        <div>
-            <h1>Be your hero</h1>
-            <p>We invite YOU to become the HERO of your own story, and every hero needs to overcome the challenges they face. Do you prefer raw spread or a commission based account? Do you use MT4 or MT5 platform? Do you trade Indices or Crypto? We are confident HeroFx will become your #1 trusted partner once you experience our world class services. Sign up today and evaluate the HERO advantage for yourself.</p>
-            <button>sign up for free</button>
-        </div>
-        <div>
-            <img src="{{ URL("images/Herofx.png")}}" alt="">
+<section class="home">
+    <section class="first">
+        <div class="hodl">
+            <div class="wording">
+                <h1>Raw spread and No commission broker</h1>
+                <p>Trade Forex, Cryptos, and indicates with confidence and enjoy
+                    a 1:500 Leverage</p>
+                <a href="#">
+                    Sign up for free
+                </a>
+            </div>
+            <div>
+                <img src="{{ URL("images/Herofx.png")}}" alt="mtscreens">
+            </div>
         </div>
     </section>
-    <section>
-        <div>
-            <img src="{{ URL("images/Herofx.png")}}" alt="">
-        </div>
-        <div>
-            <h1>METATRADER 4 AND 5</h1>
-            <p>HeroFX provides industry leading trading conditions on the Award Winning MT4 platform and MT5 platform. Our customers will soon be able to enjoy trading using Traidingview, which is coming soon.</p>
-            <button>sign up for free</button>
+    <section class="second">
+        <div class="services">
+            <div  class="inner">
+                <img src="{{ URL("images/trade-mark.png")}}" alt="mtscreens">
+                <p>latest technology</p>
+            </div>
+            <div class="inner">
+                <img src="{{ URL("images/fast-charge.png")}}" alt="mtscreens">
+                <p>fast execution</p>
+            </div>
+            <div class="inner">
+                <img src="{{ URL("images/chart-1.png")}}" alt="mtscreens">
+                <p>zero spreads</p>
+            </div>
+            <div class="inner">
+                <img src="{{ URL("images/devices.png")}}" alt="mtscreens">
+                <p>desktop, mobile & web</p>
+            </div>
         </div>
     </section>
-    <section>
-        <div>
-            <h1>TRADE WITH RAW SPREAD OR ZERO COMMISSIONS</h1>
-            <p>HeroFX offers multiple account types to meet the needs of all traders. $20 Min Deposit
-            </p>
-            <h4>$20 Minimum Deposit 1:500 Leverage</h4>
-            <h4>0.01 Minimum Lot 71 forex pairs</h4>
-            <h4>59 Stocks 30 Cryptos</h4>
-            <button>sign up for free</button>
+    <section class="power">
+        <div class="hodl">
+            <div class="heading">
+                <h1>Be your hero</h1>
+                <p>We invite YOU to become the HERO of your own story, and every hero needs to overcome the challenges they face. Do you prefer raw spread or a commission based account? Do you use MT4 or MT5 platform? Do you trade Indices or Crypto? We are confident HeroFx will become your #1 trusted partner once you experience our world class services. Sign up today and evaluate the HERO advantage for yourself.</p>
+                <a href="#">sign up for free</a>
+            </div>
+            <div>
+                <img src="{{ URL("images/Herofx.png")}}" alt="">
+            </div>
         </div>
-        <div>
-            <img src="{{ URL("images/Herofx.png")}}" alt="">
+    </section>
+    <section class="power">
+        <div class="hodl">
+            <div>
+                <img src="{{ URL("images/Herofx.png")}}" alt="">
+            </div>
+            <div class="heading">
+                <h1>METATRADER 4 AND 5</h1>
+                <p>HeroFX provides industry leading trading conditions on the Award Winning MT4 platform and MT5 platform. Our customers will soon be able to enjoy trading using Traidingview, which is coming soon.</p>
+                <a href="#">sign up for free</a>
+            </div>
+        </div>
+    </section>
+    <section class="power">
+        <div class="hodl">
+            <div class="heading">
+                <h1>TRADE WITH RAW SPREAD OR ZERO COMMISSIONS</h1>
+                <p>HeroFX offers multiple account types to meet the needs of all traders. $20 Min Deposit
+                </p>
+                <h4>$20 Minimum Deposit 1:500 Leverage</h4>
+                <h4>0.01 Minimum Lot 71 forex pairs</h4>
+                <h4>59 Stocks 30 Cryptos</h4>
+                <a href="#">sign up for free</a>
+            </div>
+            <div>
+                <img src="{{ URL("images/Herofx.png")}}" alt="">
+            </div>
         </div>
     </section>
     <section>
@@ -66,4 +81,5 @@
         <p>WHAT OUR CLIENTS HAVE TO SAY</p>
         <div classname="slider"></div>
     </section>
+</section>
 @endsection
